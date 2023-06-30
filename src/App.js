93bce6bcc115d4eb06a2badc35bd4b3df68debfb
wassim <wassim.hassiin@gmail.com>
2023-06-30 16:55:04 +0200

    sec commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Login from "pages/login/login.";
+import Home from "pages/home/home";
+import Profile from "pages/profile/profile";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  // const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/:profile/:userId" element={<Profile/>}/>
+      </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Navbar from "pages/navbar/navbar";
+
+const Home = ()=>{
+    return(
+        <Box>
+            <Navbar/>
+        </Box>
+    );
+};
+
+export default Home; 
